9a46cf950ec6b3b252953803be9c13ed9e786146
vlmnst <vale-mansueto@hotmail.com>
2023-01-14 22:13:49 -0300

    open url in a open window

--- src/controllers/check.controler.js
+++ src/controllers/check.controler.js
@@ -1,51 +1,66 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 const checkwsp = (req, res) => {
-    //SELECTORES
-//Nombre del contacto
-const div_contenedor_prueba =      '#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8',
-name_contacto_prueba =       '#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._3vPI2 > div.zoWT4 > span',
-div_de_notificacion_prueba = '#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._37FrU',
-notificacion_prueba =        '#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._37FrU > div._1i_wG > span:nth-child(1) > div'
+  //SELECTORES
+  //Nombre del contacto
+  const div_contenedor_prueba =
+      "#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8",
+    name_contacto_prueba =
+      "#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._3vPI2 > div.zoWT4 > span",
+    div_de_notificacion_prueba =
+      "#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._37FrU",
+    notificacion_prueba =
+      "#pane-side > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div > div._3OvU8 > div._37FrU > div._1i_wG > span:nth-child(1) > div";
 
-//Url wsp
-const url = 'https://web.whatsapp.com/'
+  //Url 
+  const wsChromeEndpointurl = 'ws://localhost:9222/devtools/browser/866911a8-0e1b-4bdd-8f6c-514d106448ca';
+  const url = "https://web.whatsapp.com/";
 
-//Funcion para abrir la pagina
-async function startBrowser() {
-const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage();
-return {browser, page};
-}
+  //An array of all open pages inside the Browser.
+  (async () => {
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: wsChromeEndpointurl,
+  });
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+  });
+  })();
 
-//Funcion para cerrar la pagina
-async function closeBrowser(browser) {
-return browser.close();
-}
+  //Funcion para abrir la pagina
+  async function startBrowser() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    return { browser, page };
+  }
 
-async function playTest(url) {
-try {
-  const {browser, page} = await startBrowser();
-  page.setViewport({width: 1366, height: 768});
+  //Funcion para cerrar la pagina
+  async function closeBrowser(browser) {
+    return browser.close();
+  }
 
-  await page.goto(url);
- 
-//    closeBrowser(browser)
-} catch (error) {
-  console.log(error);
-}
+  async function playTest(url) {
+    try {
+      const { browser, page } = await startBrowser();
+      page.setViewport({ width: 1366, height: 768 });
 
+      await page.goto(url);
+
+      //    closeBrowser(browser)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // (async () => {
+  //   try {
+  //     await playTest(url);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // })();
+
+  res.send({ msg: "hola" });
 };
-
-(async () => {
-try {
- await playTest(url);
-} catch (error) {
-  console.log(error);
-}
-
-})();
-    res.send({msg: 'hola'})
-}
 
 export default checkwsp;
